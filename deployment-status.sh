a0841aa89fdfe2b5d043b2cfafebad2ae7fa6236
IkenoXamos <moberlies1337@gmail.com>
2022-08-05 12:43:32 -0400

    Restructure kubernetes manifests to leverage a new ingress controller

--- deployment-status.sh
+++ deployment-status.sh
@@ -6,3 +6,8 @@ fi
 if ! kubectl rollout status deployment sample-spring-boot -n matthew-oberlies; then
     kubectl apply -f kubernetes.yml
 fi
+
+if ! git diff HEAD~1 HEAD --exit-code -s kubernetes.yml; then
+    echo "Changes to kubernetes.yml"
+    kubectl apply -f kubernetes.yml
+fi
